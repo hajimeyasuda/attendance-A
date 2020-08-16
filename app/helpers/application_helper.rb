af9a2d909df167f6a8a239aaf65c7ad69618360a
hajimeyasuda <oneswill0303@gmail.com>
2020-08-16 10:19:56 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # カスタムヘルパーを定義する
+  
+  # ページごとにタイトルを返す
+  def full_title(page_name = "") # メソッドの引数の定義
+    base_title = "AttendanceB" # 基本となるアプリケーション名を変数に代入
+    if page_name.empty? # 引数を受け取っているかの判定
+      base_title # 引数page_nameがから文字の場合はbase_titleのみ返す
+    else # 引数page_nameがから文字ではない場合
+      page_name + " | " + basic_title # 文字を連結して返す
+    end
+  end
 end
