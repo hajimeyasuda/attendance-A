88d366007fc5f03de416ebe87056879595fc542b
hajimeyasuda <oneswill0303@gmail.com>
2020-08-19 13:09:39 +0000

    5.Strong Parameters追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "作成しました。"
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+  
+  
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+
 end
 
