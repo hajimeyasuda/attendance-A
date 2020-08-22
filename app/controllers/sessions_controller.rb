dcd8a3aba7e5fefaafd6af315028d16cbae29883
hajimeyasuda <oneswill0303@gmail.com>
2020-08-22 13:59:17 +0000

    6.エラ〜メッセージの表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render :new
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to login_url
+    else
+      flash.now[:danger] = "認証に失敗しました。"
+      render :new
+    end
   end
 end
