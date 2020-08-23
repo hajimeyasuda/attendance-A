a29bc8e6f89213629a69b8a1ace8572012f20870
hajimeyasuda <oneswill0303@gmail.com>
2020-08-23 06:01:41 +0000

    6.current_userの定義

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,9 @@ module SessionsHelper
   end
   # この定義でログインをしてcreateアクション、そしてリダイレクトする準備完了
   
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
