3dcdd0c91dc77564dbec588db2dd0cec88699a93
hajimeyasuda <oneswill0303@gmail.com>
2020-08-26 08:57:53 +0000

    8.セキュリティーモデル１ログインを要求

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -23,6 +24,17 @@ class UsersController < ApplicationController
     
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_url(@user)
+    else
+      render :edit
+    end
+    
+  end
+  
   
   
   private
@@ -31,6 +43,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    # beforeフィルター
+    
+    # ログイン済みのユーザーか確認する。
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
 
 end
 
