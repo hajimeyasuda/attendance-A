e24a94171ef833b600d7e46d896e93b8ac65cf58
hajimeyasuda <oneswill0303@gmail.com>
2021-01-31 22:56:49 +0900

    ユーザ個人情報追加設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 # coding: utf-8
 
-User.create!(name: "Sample User",
-             email: "sample@email.com",
+User.create!(name: "管理者",
+             email: "admin@email.com",
              password: "password",
              password_confirmation: "password",
              admin: true)
              
-             
+User.create!(name: "上長１",
+             email: "superior_1@email.com",
+             password: "password",
+             password_confirmation: "password")
+User.create!(name: "上長２",
+             email: "superior_2@email.com",
+             password: "password",
+             password_confirmation: "password")
+
 60.times do |n|
   name = Faker::Name.name
   email = "sample-#{n+1}@email.com"
