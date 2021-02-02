8204e39a81edacb5b1a18a93633365a2cc154da9
hajimeyasuda <oneswill0303@gmail.com>
2021-02-03 02:03:22 +0900

    勤怠編集画面完成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   
   # ページごとにタイトルを返す
   def full_title(page_name = "") # メソッドの引数の定義
-    base_title = "AttendanceB" # 基本となるアプリケーション名を変数に代入
+    base_title = "Attendance-A" # 基本となるアプリケーション名を変数に代入
     if page_name.empty? # 引数を受け取っているかの判定
       base_title # 引数page_nameがから文字の場合はbase_titleのみ返す
     else # 引数page_nameがから文字ではない場合
