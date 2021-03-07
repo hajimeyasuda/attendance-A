1a1b66df83423ebb2a59f0d3a84538eea2798794
hajimeyasuda <oneswill0303@gmail.com>
2021-03-07 22:06:21 +0900

    ユーザ一覧アコーディオン表示完成

---
+++ db/migrate/20210307124300_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :uid, :string
+    add_column :users, :employee_number, :integer
+    add_column :users, :basic_work_time, :datetime
+    add_column :users, :designated_work_start_time, :datetime
+    add_column :users, :designated_work_end_time, :datetime
+  end
+end
